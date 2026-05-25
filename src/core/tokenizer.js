3cc5826dfeff6f079fa808a87dbc92c09ebae287
FuwaKodo <gameknight999credric@gmail.com>
2026-05-25 19:53:16 -0400

    format: format tokenizer constants with prettier

--- src/core/tokenizer.js
+++ src/core/tokenizer.js
@@ -1,14 +1,37 @@
 export const TT = Object.freeze({
-    KEYWORD: "KW", IDENT: "ID", NUMBER: "NUM", STRING: "STR",
-    CHAR_LIT: "CHR", OP: "OP", PUNC: "PN", EOF: "EOF"
+  KEYWORD: "KW",
+  IDENT: "ID",
+  NUMBER: "NUM",
+  STRING: "STR",
+  CHAR_LIT: "CHR",
+  OP: "OP",
+  PUNC: "PN",
+  EOF: "EOF",
 });
 
 export const KEYWORDS = new Set([
-    "int", "char", "float", "double", "void", "long", "short", "unsigned",
-    "return", "if", "else", "while", "for",
-    "sizeof", "malloc", "free", "printf", "NULL", "const", "static"
+  "int",
+  "char",
+  "float",
+  "double",
+  "void",
+  "long",
+  "short",
+  "unsigned",
+  "return",
+  "if",
+  "else",
+  "while",
+  "for",
+  "sizeof",
+  "malloc",
+  "free",
+  "printf",
+  "NULL",
+  "const",
+  "static",
 ]);
 
 export function tokenize(src) {
-    const tokens = [];
+  const tokens = [];
 }

07670c6e79c1a7bc71abb88b3a396b85c5e714aa
FuwaKodo <gameknight999credric@gmail.com>
2026-05-29 10:25:00 -0400

    feat: added matching type and values, and fixed formatting for consuming expected tokens

--- src/core/parser.js
+++ src/core/parser.js
@@ -18,21 +18,32 @@ export class Parser {
   }
 
   err(message) {
-    const token = this.getCurrentToken(); 
-    throw new Error(`Parse error at line ${token.line}: ${message}`); 
+    const token = this.getCurrentToken();
+    throw new Error(`Parse error at line ${token.line}: ${message}`);
+  }
+
+  isMatch(type, value) {
+    const token = this.getCurrentToken();
+    const matchType = token.type === type;
+    const matchValue = value === undefined || token.value === value;
+    return matchType && matchValue;
+  }
+
+  isMatchAny(type, values) {
+    return values.some((value) => this.isMatch(type, value));
   }
 
   consumeExpectedToken(type, value) {
     const token = this.getCurrentToken();
     if (type && token.type !== type) {
-      this.err(`Expected ${type} but got ${token.type} (${token.value})`)
-    }
-    
-    if (value !== undefined && token.value !== value) {
-      this.err(`Expected '${value}' but got '${token.value}'`)
+      this.err(`Expected ${type} but got ${token.type} (${token.value})`);
     }
 
-    this.position += 1; 
-    return token; 
+    if (value !== undefined && token.value !== value) {
+      this.err(`Expected '${value}' but got '${token.value}'`);
+    }
+
+    this.position += 1;
+    return token;
   }
 }

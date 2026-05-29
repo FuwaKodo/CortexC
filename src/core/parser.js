3631515a922fb448251a8b52d980f50beb3f3fd8
FuwaKodo <gameknight999credric@gmail.com>
2026-05-29 10:06:45 -0400

    feat: added parser for tokenizer

---
+++ src/core/parser.js
@@ -0,0 +1,38 @@
+"use strict";
+
+import { TOKENTYPES } from "./tokenizer.js";
+
+export class Parser {
+  constructor(tokens) {
+    this.tokens = tokens;
+    this.position = 0;
+  }
+
+  peek(offset = 0) {
+    const tokenIndex = Math.min(this.pos + offset, this.tokens.length - 1);
+    return this.tokens[tokenIndex];
+  }
+
+  getCurrentToken() {
+    return this.peek();
+  }
+
+  err(message) {
+    const token = this.getCurrentToken(); 
+    throw new Error(`Parse error at line ${token.line}: ${message}`); 
+  }
+
+  consumeExpectedToken(type, value) {
+    const token = this.getCurrentToken();
+    if (type && token.type !== type) {
+      this.err(`Expected ${type} but got ${token.type} (${token.value})`)
+    }
+    
+    if (value !== undefined && token.value !== value) {
+      this.err(`Expected '${value}' but got '${token.value}'`)
+    }
+
+    this.position += 1; 
+    return token; 
+  }
+}
